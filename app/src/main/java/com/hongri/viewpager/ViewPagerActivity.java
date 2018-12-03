c962444b24a821ad87d35784eda613500d8beefe
zhongyao <zhongyao@alibaba-inc.com>
2018-12-03 11:29:17 +0800

    添加双击时放大or缩小图片逻辑

--- app/src/main/java/com/hongri/viewpager/ViewPagerActivity.java
+++ app/src/main/java/com/hongri/viewpager/ViewPagerActivity.java
@@ -7,13 +7,20 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.hongri.viewpager.adapter.MyPagerAdapter;
 import com.hongri.viewpager.util.DataUtil;
+import com.hongri.viewpager.util.ToastUtil;
 import com.hongri.viewpager.widget.CustomImageView;
 
 /**
@@ -28,6 +35,7 @@ public class ViewPagerActivity extends AppCompatActivity {
     private CustomImageView mImageView;
     private TextView mTextView;
     private final int IMAGE_SIZE = 3;
+    private int mCurrentPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,46 @@ public class ViewPagerActivity extends AppCompatActivity {
             mImageView.setLayoutParams(mImageParams);
             mImageView.setImageResource(DataUtil.getImageResource()[i]);
 
+            /**
+             * 添加手势事件
+             */
+            final GestureDetector gestureDetector = new GestureDetector(this, new SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapConfirmed(MotionEvent e) {
+                    ToastUtil.showToast(ViewPagerActivity.this, "图片：" + mCurrentPosition + " 单击事件");
+                    return super.onSingleTapConfirmed(e);
+                }
+
+                /**
+                 * 双击时放大/缩小图片
+                 * @param e
+                 * @return
+                 */
+                @Override
+                public boolean onDoubleTap(MotionEvent e) {
+                    ToastUtil.showToast(ViewPagerActivity.this, "图片：" + mCurrentPosition + " 双击事件");
+                    if (((ImageView)(dataLists.get(mCurrentPosition))).getScaleType() != ScaleType.CENTER_CROP) {
+                        ((ImageView)(dataLists.get(mCurrentPosition))).setScaleType(ScaleType.CENTER_CROP);
+                    } else {
+                        ((ImageView)(dataLists.get(mCurrentPosition))).setScaleType(ScaleType.FIT_CENTER);
+                    }
+                    return super.onDoubleTap(e);
+                }
+
+                @Override
+                public boolean onDoubleTapEvent(MotionEvent e) {
+                    return super.onDoubleTapEvent(e);
+                }
+            });
+
+            mImageView.setOnTouchListener(new OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    gestureDetector.onTouchEvent(event);
+                    return true;
+                }
+            });
+
             dataLists.add(mImageView);
         }
 
@@ -61,7 +109,6 @@ public class ViewPagerActivity extends AppCompatActivity {
         mTextView.setText(DataUtil.getDescriptions()[0]);
         indicatorContainer.addView(mTextView);
 
-
         adapter = new MyPagerAdapter(this, dataLists);
 
         viewPager.setAdapter(adapter);
@@ -71,8 +118,18 @@ public class ViewPagerActivity extends AppCompatActivity {
 
             }
 
+            /**
+             * 滑动时初始化原始图片大小
+             * @param position
+             */
             @Override
             public void onPageSelected(int position) {
+                mCurrentPosition = position;
+                for (int i = 0; i < dataLists.size(); i++) {
+                    if (((ImageView)(dataLists.get(i))).getScaleType() != ScaleType.FIT_CENTER) {
+                        ((ImageView)(dataLists.get(i))).setScaleType(ScaleType.FIT_CENTER);
+                    }
+                }
                 mTextView.setText(DataUtil.getDescriptions()[position]);
             }
 
